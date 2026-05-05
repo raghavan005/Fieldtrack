3ced3048d59a98c93b933721b1dfbce5a7b194b3
raghavan005 <rragavan2004@gmail.com>
2026-05-05 18:52:47 +0530

    fix

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -11,7 +11,7 @@ export default function Modal({
     maxWidth = '2xl',
     closeable = true,
     onClose = () => {},
-    panelClassName = '',
+    panelClassName = 'bg-white',
     overlayClassName = 'bg-gray-500/75',
 }) {
     const close = () => {
@@ -56,7 +56,7 @@ export default function Modal({
                     leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
                     <DialogPanel
-                        className={`mb-6 transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:mx-auto sm:w-full ${maxWidthClass} ${panelClassName}`}
+                        className={`mb-6 transform overflow-hidden rounded-lg shadow-xl transition-all sm:mx-auto sm:w-full ${maxWidthClass} ${panelClassName}`}
                     >
                         {children}
                     </DialogPanel>
